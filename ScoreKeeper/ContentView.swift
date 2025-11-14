85f6812c0a463a267006df0ca16507e462170f66
Julien Hwang <jhwang@lcs.on.ca>
2025-11-14 09:23:33 -0500

    Initial Commit

---
+++ ScoreKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScoreKeeper
+//
+//  Created by Julien Hwang on 14/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScoreKeeper/ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeeperApp.swift
+//  ScoreKeeper
+//
+//  Created by Julien Hwang on 14/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
